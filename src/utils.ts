6ff87905c40eeb0a55f0c839f210d625160b03ce
Eddie Pace <eddie@pace.me.uk>
2026-04-01 14:57:55 +0100

    add hex code function

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,8 @@ import type { RGBAColor } from "./types";
 export function rgbaStr (rgba: RGBAColor) {
     return `rgba(${rgba.R ?? 0}, ${rgba.G ?? 0}, ${rgba.B ?? 0}, ${rgba.A ?? 0})`
 }
+
+export function rgbaHexCode (rgba: RGBAColor) {
+    const toHex = (n: number) => (n ?? 0).toString(16).padStart(2, '0').toUpperCase();
+    return `#${toHex(rgba.R)}${toHex(rgba.G)}${toHex(rgba.B)}${toHex(rgba.A)}`;
+}
